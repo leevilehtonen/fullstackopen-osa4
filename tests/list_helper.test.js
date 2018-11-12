9ec8c9ba8351931bd2b019b9b71b0b432a34b454
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2018-11-12 23:31:30 +0200

    4.5-4.6 done

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -54,25 +54,26 @@ const blogs = [
   }
 ];
 
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  }
+];
+
+test("dummy is called", () => {
+  const blogs = [];
+
+  const result = listHelper.dummy(blogs);
+  expect(result).toBe(1);
+});
+
 describe("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url:
-        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    }
-  ];
-  test("dummy is called", () => {
-    const blogs = [];
-
-    const result = listHelper.dummy(blogs);
-    expect(result).toBe(1);
-  });
-
   test("when list has only one blog equals the likes of that", () => {
     const result = listHelper.totalLikes(listWithOneBlog);
     expect(result).toBe(5);
@@ -85,5 +86,37 @@ describe("total likes", () => {
   test("when list is empty equals zero", () => {
     const result = listHelper.totalLikes([]);
     expect(result).toBe(0);
+  });
+});
+
+describe("favorite blog", () => {
+  test("when list has only one blog equals the favorite to that", () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toEqual(listWithOneBlog[0]);
+  });
+
+  test("when list has multiple blogs equels the favorite to the most liked one", () => {
+    const result = listHelper.favoriteBlog(blogs);
+    expect(result).toEqual(blogs[2]);
+  });
+  test("when list is empty equals null", () => {
+    const result = listHelper.favoriteBlog([]);
+    expect(result).toEqual(null);
+  });
+});
+
+describe("most blogs", () => {
+  test("when list has only one blog equals the most to that", () => {
+    const result = listHelper.mostBlogs(listWithOneBlog);
+    expect(result).toEqual({ author: "Edsger W. Dijkstra", blogs: 1 });
+  });
+
+  test("when list has multiple blogs equels the favorite to the most blogs having author", () => {
+    const result = listHelper.mostBlogs(blogs);
+    expect(result).toEqual({ author: "Robert C. Martin", blogs: 3 });
+  });
+  test("when list is empty equals null", () => {
+    const result = listHelper.mostBlogs([]);
+    expect(result).toEqual({ author: null, blogs: 0 });
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,65 @@ const totalLikes = blogs => {
   return sum;
 };
 
+const favoriteBlog = blogs => {
+  let fav = blogs.length > 0 ? blogs[0] : null;
+  blogs.forEach(blog => {
+    if (blog.likes > fav.likes) {
+      fav = blog;
+    }
+  });
+  return fav;
+};
+
+const mostBlogs = blogs => {
+  const authors = {};
+  blogs.forEach(blog => {
+    if (Object.keys(authors).includes(blog.author)) {
+      authors[blog.author]++;
+    } else {
+      authors[blog.author] = 1;
+    }
+  });
+  topAuthor = authors.length > 0 ? Object.keys(authors)[0] : null;
+  topBlogs = authors.length > 0 ? authors[0] : 0;
+  Object.keys(authors).forEach(author => {
+    if (authors[author] > topBlogs) {
+      topAuthor = author;
+      topBlogs = authors[author];
+    }
+  });
+  return {
+    author: topAuthor,
+    blogs: topBlogs
+  };
+};
+
+const mostLikes = blogs => {
+  const authors = {};
+  blogs.forEach(blog => {
+    if (Object.keys(authors).includes(blog.author)) {
+      authors[blog.author] += blog.likes;
+    } else {
+      authors[blog.author] = blog.likes;
+    }
+  });
+  topAuthor = authors.length > 0 ? Object.keys(authors)[0] : null;
+  topLikes = authors.length > 0 ? authors[0] : 0;
+  Object.keys(authors).forEach(author => {
+    if (authors[author] > topLikes) {
+      topAuthor = author;
+      topLikes = authors[author];
+    }
+  });
+  return {
+    author: topAuthor,
+    likes: topLikes
+  };
+};
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
